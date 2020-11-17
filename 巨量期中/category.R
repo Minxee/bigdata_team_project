8739253d6546e78c2a1d8696edd9d5f96c5e7768
Gary Huang <45207364+ttiverson3@users.noreply.github.com>
2020-11-17 18:12:42 +0800

    midtern

--- 巨量期中/category.R
+++ 巨量期中/category.R
@@ -13,8 +13,18 @@ C = group_by(mostViews, category) %>% summarise(likes = mean(likes), dislikes = 
 group_by(youtube, category) %>% summarise(n = n()) %>% arrange(desc(n)) %>% ggplot(aes(x = reorder(category, n), y = n, fill = category)) + geom_bar(stat = "identity") + coord_flip()
 # 什麼類別的影片總觀看數最多 : Music、Entertainment、Film & Animation、Comedy、People & Blogs
 group_by(mostViews, category) %>% summarise(total_views = sum(views)) %>% ggplot(aes(x = reorder(category, total_views), y = total_views, fill = category)) + geom_bar(stat = "identity") + coord_flip()
-# 什麼類型的影片觀眾參與度最高(喜歡數 + 觀看數 + 評論數) : Music、Entertainment、Film & Animation、Comedy、People & Blogs
-group_by(mostViews, category) %>% summarise(engagement = sum(likes + views + comment_count)) %>% arrange(desc(engagement)) %>% ggplot(aes(x = reorder(category, engagement), y = engagement, fill = category)) + geom_bar(stat = "identity") + coord_flip()
+# 什麼類型的影片觀眾參與度最高(喜歡數 + 倒讚數 + 觀看數 + 評論數) : Music、Entertainment、Film & Animation、Comedy、People & Blogs
+# group_by(mostViews, category) %>% summarise(engagement = sum(likes + dislikes +views + comment_count)) %>% arrange(desc(engagement)) %>% ggplot(aes(x = reorder(category, engagement), y = engagement, fill = category)) + geom_bar(stat = "identity") + coord_flip()
+# Min-max scaling
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+df_nor = as.data.frame(lapply(C[2:5],normalize))
+df_nor$category = C$category
+
+dfplot = df_nor[, c(1:5)] %>% gather(key, value, -category)
+ggplot(dfplot, aes(x = category)) + geom_col(aes(y = value, fill = key), position = "fill", stat = 'identity') + labs(y = "proportion") + theme(axis.text.x = element_text(angle = 90))
 
 # 什麼類別的影片平均上熱門時間最久 : Shows、Gaming、Music、Film & Animation、Howto & Style
 group_by(youtube, category) %>% summarise(days = mean(trending_days)) %>% ggplot(aes(x = reorder(category, days), y = days, fill = category)) + geom_bar(stat = "identity") + coord_flip()
